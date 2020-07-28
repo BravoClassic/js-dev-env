84f41b7ac6c59d06fda188f2001ee4c56152b348
BravoClassic <gerald.akorli@gmail.com>
2020-07-28 02:20:38 +0000

    made some changes

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -7,11 +7,11 @@ var app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
 
 app.listen(port, (err) => {
     if (err)
         console.log(err);
     else
         open(`http://localhost:${port}`);
-})
+});
